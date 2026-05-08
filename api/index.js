53ef8373cbbddf20b940f2b924e7c4a9789d6030
imloafy <velocity4293@gmail.com>
2026-05-08 11:54:05 +0530

    refactor: delegate API request handling to server-index module

--- api/index.js
+++ api/index.js
@@ -1,7 +1,3 @@
-module.exports = (req, res) => {
-  res.status(200).json({ 
-    status: "online", 
-    message: "Dreamy API is alive!",
-    environment: process.env.NODE_ENV
-  });
-};
+const app = require("./server-index");
+
+module.exports = app;
